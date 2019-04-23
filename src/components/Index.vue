544457e78e22df32fb2255a26de3d380c4588038
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-04-24 01:46:18 +0200

    Working on welcome area

---
+++ src/components/Index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card-contanier">
+    <div class="card">
+      <div class="card-body">
+        <h2 class="text-center">Chat application</h2>
+        <form class="card-user-info" @submit.prevent="openChat()">
+          <label for="user-name">You name:</label>
+          <input class="form-control" type="text" name="user-name" v-model="name">
+          <button class="btn btn-info">Visit Chat</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      name: null
+    };
+  },
+  methods: {
+    openChat() {}
+  }
+};
+</script>
+
+<style scoped>
+.card-contanier {
+  padding: 20px;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100vh;
+}
+.card-contanier,
+.card-user-info {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.card {
+  margin: 0 auto;
+}
+.card-user-info {
+  flex-direction: column;
+  max-width: 400px;
+  margin: 1rem auto 0;
+}
+.form-control {
+  margin-bottom: 0.5rem;
+}
+
+@media screen and (min-width: 600px) {
+  .card {
+    width: 500px;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false
 
@@ -10,6 +12,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
