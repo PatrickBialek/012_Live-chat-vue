50ee28b4614a29747ab79c319325e536be126587
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-04-25 13:05:51 +0200

    Working on date formating

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -19,6 +19,7 @@
 <script>
 import NewMessage from "@/components/NewMessage";
 import db from "@/firebase/init";
+import moment from "moment";
 
 export default {
   name: "Chat",
@@ -43,7 +44,7 @@ export default {
             id: doc.id,
             name: doc.data().name,
             content: doc.data().content,
-            timestamp: doc.data().timestamp
+            timestamp: moment(doc.data().timestamp).format("lll")
           });
         }
       });
